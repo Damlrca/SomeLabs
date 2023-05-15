203f733bdc041c265971478da1456dfd2478ac4a
Damlrca <sir.damirca@yandex.ru>
2023-05-15 22:30:48 +0300

    AVLtree class!

---
+++ AVLtree/AVLtree.cpp
@@ -0,0 +1,122 @@
+#include "AVLtree.h"
+
+//  small right rotation
+//     cur       l
+//     / \      / \
+//    l   C -> A  cur
+//   / \          / \
+//  A   B        B   C
+
+Node* AVLtree::_small_right_rotation(Node* cur) {
+	Node* l = cur->left;
+	Node* A = l->left;
+	Node* B = l->right;
+	Node* C = cur->right;
+
+	l->left = A;
+	l->right = cur;
+	cur->left = B;
+	cur->right = C;
+
+	int temp = 0;
+	if (B) temp = B->height;
+	if (C) temp = std::max(temp, C->height);
+	cur->height = temp + 1;
+	temp = cur->height;
+	if (A) temp = std::max(temp, A->height);
+	l->height = temp + 1;
+
+	return l;
+}
+
+//  small left rotation
+//   cur           r
+//   / \          / \
+//  A   r   ->  cur  C
+//     / \      / \
+//    B   C    A   B
+
+Node* AVLtree::_small_left_rotation(Node* cur) {
+	Node* r = cur->right;
+	Node* A = cur->left;
+	Node* B = r->left;
+	Node* C = r->right;
+
+	r->left = cur;
+	r->right = C;
+	cur->left = A;
+	cur->right = B;
+
+	int temp = 0;
+	if (A) temp = A->height;
+	if (B) temp = std::max(temp, B->height);
+	cur->height = temp + 1;
+	temp = cur->height;
+	if (C) temp = std::max(temp, C->height);
+	r->height = temp + 1;
+
+	return r;
+}
+
+Node* AVLtree::_big_right_rotation(Node* cur) {
+	cur->left = _small_left_rotation(cur->left);
+	return _small_right_rotation(cur);
+}
+
+Node* AVLtree::_big_left_rotation(Node* cur) {
+	cur->right = _small_right_rotation(cur->right);
+	return _small_left_rotation(cur);
+}
+
+int AVLtree::get_balance(Node* cur) {
+	int left_h = 0;
+	int right_h = 0;
+	if (cur->left)
+		left_h = cur->left->height;
+	if (cur->right)
+		right_h = cur->right->height;
+	return right_h - left_h;
+}
+
+void AVLtree::balance(std::stack<Node*>& path, Node* cur) {
+	while (!path.empty()) {
+		Node* parent = path.top(); path.pop();
+		int b = get_balance(cur);
+		Node* tmp = cur;
+		if (b == 2) {
+			int b_r = get_balance(cur->right);
+			if (b_r > 0)
+				tmp = _small_left_rotation(cur);
+			else
+				tmp = _big_left_rotation(cur);
+		}
+		else if (b == -2) {
+			int b_l = get_balance(cur->left);
+			if (b_l < 0)
+				tmp = _small_right_rotation(cur);
+			else
+				tmp = _big_right_rotation(cur);
+		}
+		if (parent->right == cur)
+			parent->right = tmp;
+		else
+			parent->left = tmp;
+		cur = parent;
+	}
+
+	int b = get_balance(root);
+	if (b == 2) {
+		int b_r = get_balance(root->right);
+		if (b_r > 0)
+			root = _small_left_rotation(root);
+		else
+			root = _big_left_rotation(root);
+	}
+	else if (b == -2) {
+		int b_l = get_balance(root->left);
+		if (b_l < 0)
+			root = _small_right_rotation(root);
+		else
+			root = _big_right_rotation(root);
+	}
+}
---
+++ AVLtree/AVLtree.h
@@ -0,0 +1,103 @@
+#pragma once
+#include <iostream>
+#include <algorithm>
+#include <stack>
+
+struct Node {
+	Node* left;
+	Node* right;
+	int height;
+	int value;
+	Node(int _val) : Node(_val, nullptr, nullptr) {}
+	Node(int _val, Node* _left, Node* _right) : value(_val), left(_left), right(_right) {
+		height = 0;
+		if (left)
+			height = std::max(height, left->height);
+		if (right)
+			height = std::max(height, right->height);
+		height++;
+	}
+	Node(const Node& _node) {
+		value = _node.value;
+		height = _node.height;
+		if (_node.left)
+			left = new Node(*_node.left);
+		else
+			left = nullptr;
+		if (_node.right)
+			right = new Node(*_node.right);
+		else
+			right = nullptr;
+	}
+	~Node() {
+		delete left;
+		delete right;
+	}
+};
+
+class AVLtree {
+private:
+	Node* root = nullptr;
+	Node* _small_right_rotation(Node* cur);
+	Node* _small_left_rotation(Node* cur);
+	Node* _big_right_rotation(Node* cur);
+	Node* _big_left_rotation(Node* cur);
+	int get_balance(Node* cur);
+	void balance(std::stack<Node*>& path, Node* cur);
+	void _debug_write(Node* n) {
+		if (n) {
+			_debug_write(n->left);
+			std::cout << n->value << " ";
+			_debug_write(n->right);
+		}
+	}
+public:
+	AVLtree() : root(nullptr) {}
+	AVLtree(const AVLtree& tr) : root(nullptr) {}
+	void add_value(int value) {
+		if (root) {
+			std::stack<Node*> path;
+			path.push(root);
+			while (true)
+			{
+				if (value < path.top()->value) {
+					if (path.top()->left) {
+						path.push(path.top()->left);
+					}
+					else {
+						path.top()->left = new Node(value);
+						balance(path, path.top()->left);
+						return;
+					}
+				}
+				else if (value > path.top()->value) {
+					if (path.top()->right) {
+						path.push(path.top()->right);
+					}
+					else {
+						path.top()->right = new Node(value);
+						balance(path, path.top()->right);
+						return;
+					}
+				}
+				else { // path.top()->value == value
+					return;
+				}
+			}
+		}
+		else {
+			root = new Node(value);
+		}
+	}
+	void _debug_write() {
+		_debug_write(root);
+		std::cout << std::endl;
+	}
+	void clear() {
+		delete root;
+		root = nullptr;
+	}
+	~AVLtree() {
+		clear();
+	}
+};
