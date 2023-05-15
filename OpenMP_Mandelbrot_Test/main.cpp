25536184e3cfbd8548cda3e533908b6f67da6bad
Damlrca <sir.damirca@yandex.ru>
2023-05-15 20:47:11 +0300

    BitmapData

--- OpenMP_Mandelbrot_Test/main.cpp
+++ OpenMP_Mandelbrot_Test/main.cpp
@@ -2,12 +2,25 @@
 #include <chrono>
 #include <algorithm>
 
-constexpr int width = 1000;
-constexpr int height = 1000;
-constexpr int iterations = 1000;
+constexpr int width = 3000;
+constexpr int height = 3000;
+constexpr int iterations = 3000;
+
+// TEST
+// width: 3000
+// height : 3000
+// iterations : 3000
+// without_pragma_time : 33102ms
+// with_pragma_time : 8762ms
 
 void mandlebrot_without_pragma() {
 	System::Drawing::Bitmap^ bm = gcnew System::Drawing::Bitmap(width, height);
+	System::Drawing::Imaging::BitmapData^ bd = bm->LockBits(
+		System::Drawing::Rectangle(0, 0, width, height),
+		System::Drawing::Imaging::ImageLockMode::WriteOnly,
+		System::Drawing::Imaging::PixelFormat::Format24bppRgb);
+	char* scan = (char*)bd->Scan0.ToPointer();
+	int stride = bd->Stride;
 	for (int i = 0; i < width; i++) {
 		for (int j = 0; j < height; j++) {
 			double x = 3.0 / width * i  - 2.0;
@@ -23,14 +36,25 @@ void mandlebrot_without_pragma() {
 			}
 			int b = cnt == iterations ? 0 : 255;
 			int rg = cnt == iterations ? 0 : 255 - std::min(250 * cnt / 50, 255);
-			bm->SetPixel(i, j, System::Drawing::Color::FromArgb(rg, rg, b));
+
+			char* curpos = scan + j * stride + i * 3;
+			*curpos = b; curpos++;
+			*curpos = rg; curpos++;
+			*curpos = rg;
 		}
 	}
+	bm->UnlockBits(bd);
 	bm->Save("mandel_without_pr.png");
 }
 
 void mandlebrot_with_pragma() {
 	System::Drawing::Bitmap^ bm = gcnew System::Drawing::Bitmap(width, height);
+	System::Drawing::Imaging::BitmapData^ bd = bm->LockBits(
+		System::Drawing::Rectangle(0, 0, width, height),
+		System::Drawing::Imaging::ImageLockMode::WriteOnly,
+		System::Drawing::Imaging::PixelFormat::Format24bppRgb);
+	char* scan = (char*)bd->Scan0.ToPointer();
+	int stride = bd->Stride;
 	#pragma omp parallel for
 	for (int i = 0; i < width; i++) {
 		for (int j = 0; j < height; j++) {
@@ -47,23 +71,30 @@ void mandlebrot_with_pragma() {
 			}
 			int b = cnt == iterations ? 0 : 255;
 			int rg = cnt == iterations ? 0 : 255 - std::min(250 * cnt / 50, 255);
-			#pragma omp critical
-			bm->SetPixel(i, j, System::Drawing::Color::FromArgb(rg, rg, b));
+
+			char* curpos = scan + j * stride + i * 3;
+			*curpos = b; curpos++;
+			*curpos = rg; curpos++;
+			*curpos = rg;
 		}
 	}
+	bm->UnlockBits(bd);
 	bm->Save("mandel_with_pr.png");
 }
 
 int main() {
+	std::cout << "width: " << width << std::endl;
+	std::cout << "height: " << height << std::endl;
+	std::cout << "iterations: " << iterations << std::endl;
 	auto start_time = std::chrono::system_clock::now();
 
 	mandlebrot_without_pragma();
 	auto without_pragma_time = std::chrono::system_clock::now();
-	std::cout << "without_pragma_time: " << std::chrono::duration_cast<std::chrono::milliseconds>(without_pragma_time - start_time).count() << "ms\n";
+	std::cout << "without_pragma_time: " << std::chrono::duration_cast<std::chrono::milliseconds>(without_pragma_time - start_time).count() << "ms" << std::endl;
 
 	mandlebrot_with_pragma();
 	auto with_pragma_time = std::chrono::system_clock::now();
-	std::cout << "with_pragma_time: " << std::chrono::duration_cast<std::chrono::milliseconds>(with_pragma_time - without_pragma_time).count() << "ms\n";
+	std::cout << "with_pragma_time: " << std::chrono::duration_cast<std::chrono::milliseconds>(with_pragma_time - without_pragma_time).count() << "ms" << std::endl;
 	
 	return 0;
 }
